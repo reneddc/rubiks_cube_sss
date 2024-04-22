b0c872019bdb07d5ba28b2e4712776c9bc8538af
rene-ad <rene.diaz@assuresoftbolivia.com>
2024-04-22 17:21:50 -0400

    R_down action done

--- examples.py
+++ examples.py
@@ -1,5 +1,4 @@
 import numpy as np
-from Faces import Faces
 
 u_face = np.array([[0,0,'u'], [1,1,'u'], [2,2,'u']]) #Red face
 l_face = np.array([[3,3,'l'], [4,4,'l'], [5,5,'l']])#Green face
@@ -10,7 +9,7 @@ b_face = np.array([['b',12,12], ['b',13,13], ['b',14,14]])  #White face
 
 rubiks_cube = np.array([u_face, l_face, f_face, d_face, r_face, b_face]) 
 
-r_actions = [0,2,3,5] #u,f,d,b -> up[2,3,5,0]
+r_actions = [0,2,3,5] #u,f,d,b -> up[2,3,5,0] down [5,0,2,3]
 l_actions = [0,2,3,5]
 
 u_actions = [0,1,4,5]
@@ -34,7 +33,20 @@ def r_up(rubiks_cube):
     new_cube = rubiks_cube.copy()
     new_cube[4] = turn_face(new_cube[4]) # rotando la cara
     rotation_list = np.roll(r_actions,shift = -1) #setea como debe ser el orden de cambio
+    print(rotation_list)
     for i in range(4):
+        new_cube[r_actions[i]][:, -1] = rubiks_cube[rotation_list[i]][:, -1]
+    new_cube[5] = new_cube[5][::-1,::-1] #back_face
+    return new_cube
+
+def r_down(rubiks_cube):
+    rubiks_cube[5] = rubiks_cube[5][::-1,::-1] #back_face
+    new_cube = rubiks_cube.copy()
+    new_cube[4] = turn_face_prime(new_cube[4]) # rotando la cara
+    rotation_list = np.roll(r_actions,shift = 1) #setea como debe ser el orden de cambio
+    print(rotation_list)
+    for i in range(4):
+        print()
         new_cube[r_actions[i]][:, -1] = rubiks_cube[rotation_list[i]][:, -1]
     new_cube[5] = new_cube[5][::-1,::-1] #back_face
     return new_cube
@@ -45,7 +57,10 @@ def r_up(rubiks_cube):
 # print(result)
 # result = turn_face_prime(rubiks_cube[0])
 # print(result)
-
-faces = Faces()
-print(faces)
-print(r_up(rubiks_cube))
+print("R_UP------------------")
+rubiks_cube = r_up(rubiks_cube)
+print(rubiks_cube)
+ 
+print()
+print("R_DOWN------------------")
+print(r_down(rubiks_cube))
